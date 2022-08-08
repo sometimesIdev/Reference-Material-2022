88f1359895d2e721e0f6a0a1257ed08fee724ccf
Pat Butler <pcbutler@telus.net>
2022-08-08 12:56:16 -0700

    Initial Commit

---
+++ Reference Material/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Reference Material
+//
+//  Created by Pat Butler on 2022-08-08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Reference Material/Reference_MaterialApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reference_MaterialApp.swift
+//  Reference Material
+//
+//  Created by Pat Butler on 2022-08-08.
+//
+
+import SwiftUI
+
+@main
+struct Reference_MaterialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
